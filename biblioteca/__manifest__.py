b18d9f12e271acfa2a974a5f0fd7cc9236f408e2
tres7337 <42941027+tres7337@users.noreply.github.com>
2023-07-05 12:46:16 +0200

    Comienzo sesion 4

---
+++ biblioteca/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Biblioteca',
+
+    'summary': 'Biblioteca Virtual',
+
+    'description': 'Módulo de una biblioteca virtual para ir cogiendo los conceptos de Odoo paso a paso',
+
+    'author': 'Curso Odoo',
+    'contributors': [
+                     "Jesús Plaza <jesus.plaza@altia.es>"
+                     ],
+    'website': "http://www.altia.es",
+    'category': 'Altia Desarrollo',
+    'version': '14.0.1.0.0',
+    'installable': True,
+    'depends': ['base',
+                ],
+    
+    # always loaded
+    'data': [                    
+    ],
+    'qweb': [
+        
+    ],
+    # only loaded in demonstration mode
+}
